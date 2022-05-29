533d8460e80a53751d0e1acd2078d2c33ad69a8b
plotok11 <fajarfyto11@gmail.com>
2022-05-30 00:20:48 +0700

    Added room api

---
+++ src/models/rooms.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const RoomSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    maxPeople: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    roomNumbers: [{
+        number: Number,
+        unavailableDates: {type: [Date]}
+    }],
+
+}
+, {timestamps: true}
+)
+
+module.exports = mongoose.model('rooms', RoomSchema)
--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const { createRoom, updateRoomById, deleteRoomById, getRoomById, getAllRoom } = require('../controllers/room')
+const { verifyAdmin } = require('../middleware/verifyToken')
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.send('This is rooms Endpoint')
-})
 
-router.get("/detail-room", (req, res) => {
-    res.send('This is detail rooms Endpoint')
-})
+// CREATE
+router.post('/:hotelId', verifyAdmin, createRoom)
+
+// UPDATE
+router.put('/:id', verifyAdmin, updateRoomById)
+
+// DELETE
+router.delete('/:id/:hotelId', verifyAdmin, deleteRoomById)
+
+// GET BY ID
+router.get('/:id/', getRoomById)
+
+// GET ALL
+router.get('/', getAllRoom)
+
+
 
 module.exports = router
